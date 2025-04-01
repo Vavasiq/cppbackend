948e2dddfd9d3ffaef0f0d1e744bd789e24447b3
Vavasiq <vavasiq@gmail.com>
2025-04-02 01:01:09 +0300

    Перенес реализацию методов http_server из заголовочного файла в cpp-файл

--- sprint1/problems/final_task/solution/src/http_server.cpp
+++ sprint1/problems/final_task/solution/src/http_server.cpp
@@ -2,6 +2,56 @@
 
 namespace http_server {
 
-// Разместите здесь реализацию http-сервера, взяв её из задания по разработке асинхронного сервера
+void ReportError(beast::error_code ec, std::string_view what) {
+    using namespace std::literals;
+    std::cerr << what << ": "sv << ec.message() << std::endl;
+}
 
-}  // namespace http_server
+SessionBase::SessionBase(tcp::socket&& socket)
+    : stream_(std::move(socket)) {
+}
+
+SessionBase::~SessionBase() = default;
+
+void SessionBase::Run() {
+    net::dispatch(stream_.get_executor(),
+                  beast::bind_front_handler(&SessionBase::Read, GetSharedThis()));
+}
+
+void SessionBase::Read() {
+    using namespace std::literals;
+    request_ = {};
+    stream_.expires_after(30s);
+    http::async_read(stream_, buffer_, request_,
+                     beast::bind_front_handler(&SessionBase::OnRead, GetSharedThis()));
+}
+
+void SessionBase::OnRead(beast::error_code ec, std::size_t bytes_read) {
+    using namespace std::literals;
+    if (ec == http::error::end_of_stream) {
+        return Close();
+    }
+    if (ec) {
+        return ReportError(ec, "read"sv);
+    }
+    HandleRequest(std::move(request_));
+}
+
+void SessionBase::OnWrite(bool close, beast::error_code ec, std::size_t bytes_written) {
+    using namespace std::literals;
+    if (ec) {
+        return ReportError(ec, "write"sv);
+    }
+    if (close) {
+        return Close();
+    }
+    Read();
+}
+
+void SessionBase::Close() {
+    beast::error_code ec;
+    stream_.socket().shutdown(tcp::socket::shutdown_send, ec);
+    ReportError(ec, "close");
+}
+
+} // namespace http_server
